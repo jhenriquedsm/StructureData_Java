1ea471e2c14f236a70c8e9d0db62076568025a0e
jhenriquedsm <jhsilvamata@gmail.com>
2023-09-14 15:20:24 -0300

    commit initial

---
+++ src/com/jhenriquedsm/estruturadados/teste/vetor/Aula02.java
@@ -0,0 +1,12 @@
+package com.jhenriquedsm.estruturadados.teste.vetor;
+
+import com.jhenriquedsm.estruturadados.vetor.Vetor;
+
+public class Aula02 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(5);
+
+        int i = 0;
+        i++;
+    }
+}
---
+++ src/com/jhenriquedsm/estruturadados/teste/vetor/Aula03.java
@@ -0,0 +1,11 @@
+package com.jhenriquedsm.estruturadados.teste.vetor;
+
+import com.jhenriquedsm.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(5);
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+    }
+}
