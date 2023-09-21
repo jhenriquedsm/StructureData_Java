6e708de97d97053b96e7a1e365c4115b8358ab43
jhenriquedsm <jhsilvamata@gmail.com>
2023-09-21 13:30:41 -0300

    Generalizando o tipo dos elementos

--- src/com/jhenriquedsm/estruturadados/teste/vetor/Contato.java
+++ src/com/jhenriquedsm/estruturadados/teste/vetor/Contato.java
@@ -1,5 +1,7 @@
 package com.jhenriquedsm.estruturadados.teste.vetor;
 
+import com.jhenriquedsm.estruturadados.vetor.VetorObjetos;
+
 import java.util.Objects;
 
 public class Contato {
@@ -55,5 +57,32 @@ public class Contato {
     @Override
     public int hashCode() {
         return Objects.hash(nome, telefone, email);
+    }
+
+    public static class Aula10 {
+        public static void main(String[] args) {
+            VetorObjetos vetor = new VetorObjetos(3);
+
+            Contato c1 = new Contato("Contato 1", "4444-5555", "contato1@email.com");
+            Contato c2 = new Contato("Contato 2", "6666-7777", "contato2@email.com");
+            Contato c3 = new Contato("Contato 3", "8888-9999", "contato3@email.com");
+            Contato c4 = new Contato("Contato 1", "4444-5555", "contato1@email.com");
+
+            vetor.adiciona(c1);
+            vetor.adiciona(c2);
+            vetor.adiciona(c3);
+
+            System.out.println("Tamanho = " + vetor.tamanho());
+
+            int pos = vetor.busca(c4);
+            if(pos > -1){
+                System.out.println("Elemento encontrado!");
+            } else {
+                System.out.println("Elemento não encontrado!");
+            }
+
+            System.out.println(vetor);
+
+        }
     }
 }
